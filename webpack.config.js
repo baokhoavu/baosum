3819665bc6d7af761fd26b0d80c79d786a947ba0
Khoa <tealkrysta92@gmail.com>
2020-05-10 18:37:39 -0700

    webpack changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,70 +1,46 @@
-// module.exports = {
-//     entry: "./src/index.tsx",
-//     module: {
-//         rules: [
-//             {
-//                 test: /\.(js|jsx|tsx)$/,
-//                 exclude: /node_modules/,
-//                 use: {
-//                     loader: "babel-loader"
-//                 }
-//             }
-//         ]
-//     }
-// };
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
 
 module.exports = {
     entry: "./src/index.tsx",
     mode: "production",
-  
+
     // Enable sourcemaps for debugging webpack's output.
     devtool: "source-map",
-  
+
     // Add '.ts' and '.tsx' as resolvable extensions.
     resolve: {
-        extensions: ['', '.js', '.jsx', '.ts', '.tsx']
+        extensions: [ '.tsx', '.ts', '.js' ],
     },
 
-    // module: {
-    //     loaders: [
-    //       {
-    //         test: /\.jsx?$/,
-    //         loader: 'babel',
-    //         exclude: /node_modules/,
-    //         query: {
-    //           cacheDirectory: true,
-    //           presets: ['react', 'es2015']
-    //         }
-    //       }
-    //     ]
-    //   }
-  
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx|ts|tsx)$/,
-          exclude: /node_modules/,
-          use: [
-            {
-              loader: "ts-loader"
-            }
-          ]
-        },
-        // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-        {
-          enforce: "pre",
-          test: /\.(js|jsx|tsx)$/,
-          loader: "source-map-loader"
-        }
-      ]
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
     },
-  
-    // When importing a module whose path matches one of the following, just
-    // assume a corresponding global variable exists and use that instead.
-    // This is important because it allows us to avoid bundling all of our
-    // dependencies, which allows browsers to cache those libraries between builds.
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            }, 
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                loader: "ts-loader"
+            }
+        ]
+    },
+
+    plugins: [new MiniCssExtractPlugin()],
+    
+
+    // When importing a module whose path matches one of the following, just assume
+    // a corresponding global variable exists and use that instead. This is
+    // important because it allows us to avoid bundling all of our dependencies,
+    // which allows browsers to cache those libraries between builds.
     externals: {
-      react: "React",
-      "react-dom": "ReactDOM"
+        react: "React",
+        "react-dom": "ReactDOM"
     }
-  };
+};
